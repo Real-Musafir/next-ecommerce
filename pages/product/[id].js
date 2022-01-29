f6b196c578dc56722b78406b464105202c2d2aee
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-01-29 18:51:05 +0600

    add product to cart from details page

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Head from "next/head";
 import { getData } from "../../utils/fetchData";
+import { DataContext } from "../../store/GlobalState";
+import { addToCart } from "../../store/Actions";
 
 const DetailProduct = (props) => {
   const [product] = useState(props.product);
   const [tab, setTab] = useState(0);
+
+  const { state, dispatch } = useContext(DataContext);
+  const { cart } = state;
 
   const isActive = (index) => {
     if (tab === index) {
@@ -56,7 +61,11 @@ const DetailProduct = (props) => {
         <div className="my-2">{product.description}</div>
         <div className="my-2">{product.content}</div>
 
-        <button type="button" className="btn btn-dark d-block my-3 px-5">
+        <button
+          onClick={() => dispatch(addToCart(product, cart))}
+          type="button"
+          className="btn btn-dark d-block my-3 px-5"
+        >
           Buy
         </button>
       </div>
