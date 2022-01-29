27f31bad5ddab53d3e22d0486660b2dc3aa8a1a8
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-01-29 19:10:59 +0600

    set an image in cart page if cart is empty

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,22 @@
+import Head from "next/head";
+import { useContext } from "react";
+import { DataContext } from "../store/GlobalState";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const { state, dispatch } = useContext(DataContext);
+  const { cart } = state;
+
+  if (cart.length === 0)
+    return <img className="img-responsive w-100" src="/empty_cart.jpeg" />;
+
+  return (
+    <div>
+      <Head>
+        <title>Cart Page</title>
+      </Head>
+      Cart
+    </div>
+  );
 };
 
 export default Cart;
